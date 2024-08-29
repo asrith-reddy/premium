b3129808c5de8265c7ba74a2a0c9efdace258d83
asrith-reddy <76733972+asrith-reddy@users.noreply.github.com>
2024-08-29 18:29:59 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -702,13 +702,8 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/Asrith_reddy'>VJ</a>
 
--Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
-
--Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
-
-- Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
 """
 
     RENAME_TXT = """
